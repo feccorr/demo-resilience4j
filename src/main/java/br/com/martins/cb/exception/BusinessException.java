3815f9520234bb8b1443989f068ce77fdd1b20ec
fernando <fernandocmar@gmail.com>
2020-08-27 18:59:45 -0300

    clean

--- src/main/java/br/com/martins/cb/exception/BusinessException.java
+++
@@ -1,8 +0,0 @@
-package br.com.martins.cb.exception;
-
-public class BusinessException extends RuntimeException {
-
-    public BusinessException(String message) {
-        super(message);
-    }
-}
