3218eb3f33e15c4ae16b0038f2e124065ef2423b
fernando <fernandocmar@gmail.com>
2020-09-02 20:11:00 -0300

    Add prometheus config file

--- orderManagementApp/src/main/java/com/martins/orderManagementApp/service/resilience4j/UserRegistrationResilience4j.java
+++ orderManagementApp/src/main/java/com/martins/orderManagementApp/service/resilience4j/UserRegistrationResilience4j.java
@@ -27,8 +27,8 @@ public class UserRegistrationResilience4j {
 
     @CircuitBreaker(name = "service1", fallbackMethod = "fallbackForRegisterSeller")
     @RateLimiter(name = "service1", fallbackMethod = "rateLimiterfallback")
-    @Retry(name = "retryService1", fallbackMethod = "retryfallback")
     @Bulkhead(name = "bulkheadService1", fallbackMethod = "bulkHeadFallback")
+    @Retry(name = "retryService1", fallbackMethod = "retryfallback")
     public String  registerSeller(SellerDto sellerDto) {
         String response = restTemplate.postForObject("/addSeller", sellerDto, String.class);
         return response;
